27a38d13f490cda0db530e3ab2b6ff33152c8ad0
cdanette <lubrek@bk.ru>
2022-09-03 20:32:08 +0300

    add loginFragment

--- app/src/main/java/ru/cordyapp/tinimal/MainActivity.kt
+++ app/src/main/java/ru/cordyapp/tinimal/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.cordyapp.tinimal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ru/cordyapp/tinimal/presentation/auth/AuthFragment.kt
@@ -0,0 +1,7 @@
+package ru.cordyapp.tinimal.presentation.auth
+
+import androidx.fragment.app.Fragment
+import ru.cordyapp.tinimal.R
+
+class AuthFragment: Fragment() {
+}
