f1524543fb17ccd955cb00ece9c2a03b0d4f5c90
mdude <kamal.valenok@gmail.com>
2022-09-10 21:42:09 +0300

    feat/okhttp: add TinimalApi

--- app/src/main/java/ru/cordyapp/tinimal/domain/repository/AuthorizationRepository.kt
+++ app/src/main/java/ru/cordyapp/tinimal/domain/repository/AuthorizationRepository.kt
@@ -1,10 +1,11 @@
 package ru.cordyapp.tinimal.domain.repository
 
 import ru.cordyapp.tinimal.data.remote.DTOmodels.AuthDTO
+import ru.cordyapp.tinimal.data.remote.DTOmodels.TokenDTO
 import ru.cordyapp.tinimal.data.remote.DTOmodels.UserAuthDTO
 
 interface AuthorizationRepository {
-    suspend fun postAuthenticate(authDTO: AuthDTO): String
+    suspend fun postAuthenticate(authDTO: AuthDTO): TokenDTO
 
-    suspend fun postCreateUser(userAuthDTO: UserAuthDTO): String
+    suspend fun postCreateUser(userAuthDTO: UserAuthDTO): TokenDTO
 }
