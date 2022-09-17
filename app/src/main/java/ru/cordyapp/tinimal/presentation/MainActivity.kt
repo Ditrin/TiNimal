aa71f4b4ac83f657488bd4f833f4776165e40f81
mdude <kamal.valenok@gmail.com>
2022-09-17 19:41:14 +0300

    fix/navigation: fix navGraph

--- app/src/main/java/ru/cordyapp/tinimal/presentation/MainActivity.kt
+++ app/src/main/java/ru/cordyapp/tinimal/presentation/MainActivity.kt
@@ -2,15 +2,53 @@ package ru.cordyapp.tinimal.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import ru.cordyapp.tinimal.R
+import ru.cordyapp.tinimal.databinding.ActivityMainBinding
 import ru.cordyapp.tinimal.presentation.login.LoginFragment
+import ru.cordyapp.tinimal.utils.SharedPref
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHost = findViewById<View>(R.id.tinimalNavHostFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.tinimalNavHostFragment)
+
+        val graph = navController.graph
+        if (SharedPref.authToken.isNullOrBlank())
+            graph.setStartDestination(R.id.authFragment)
+        else
+            graph.setStartDestination(R.id.mainFragment)
+
+        navController.graph = graph
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id){
+                R.id.authFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.loginFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                else -> bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/cordyapp/tinimal/presentation/favourites/FavouritesFragment.kt
@@ -0,0 +1,7 @@
+package ru.cordyapp.tinimal.presentation.favourites
+
+import androidx.fragment.app.Fragment
+import ru.cordyapp.tinimal.R
+
+class FavouritesFragment : Fragment(R.layout.fragment_favorites) {
+}
