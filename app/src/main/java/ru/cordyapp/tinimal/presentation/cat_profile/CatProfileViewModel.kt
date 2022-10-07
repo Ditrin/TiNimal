bd8b00c8614ef3b3414d3acf1e348666a0696e03
mdude <kamal.valenok@gmail.com>
2022-10-07 21:03:13 +0300

    feat/cat_profile: add CatProfile

--- app/src/main/java/ru/cordyapp/tinimal/presentation/cat_profile/CatProfileViewModel.kt
+++ app/src/main/java/ru/cordyapp/tinimal/presentation/cat_profile/CatProfileViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import ru.cordyapp.tinimal.data.remote.DTOmodels.CatDTO
+import ru.cordyapp.tinimal.data.remote.DTOmodels.CatInfoDTO
 import ru.cordyapp.tinimal.domain.use_case.GetCatByIdUseCase
 import javax.inject.Inject
 
@@ -14,8 +15,8 @@ import javax.inject.Inject
 class CatProfileViewModel @Inject constructor(private val getCatByIdUseCase: GetCatByIdUseCase) :
     ViewModel() {
 
-    private val catLiveData = MutableLiveData<CatDTO>()
-    val cat: LiveData<CatDTO> = catLiveData
+    private val catLiveData = MutableLiveData<CatInfoDTO>()
+    val cat: LiveData<CatInfoDTO> = catLiveData
 
     private val isSuccessLiveData = MutableLiveData<Boolean>()
     val isSuccess: LiveData<Boolean> = isSuccessLiveData
